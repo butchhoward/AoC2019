886967cde132ce5f187ed1cb5dc9be9522a6a3d2
Butch Howard <butchhoward@gmail.com>
2019-12-07 11:06:53 -0500

    make a better test runner

--- cpp/src_test/test_main.cpp
+++ cpp/src_test/test_main.cpp
@@ -1,14 +1,43 @@
 #include "int_code_test.h"
 
+#include <string>
+#include <functional>
+#include <vector>
+#include <iostream>
+
 #include "day02_test.h"
 #include "day03_test.h"
+#include "day05_test.h"
 
+
+typedef struct Test
+{
+    std::string description;
+    std::function<bool()> test;
+} Test;
+
+typedef std::vector<Test> Tests;
 
 int main()
 {
-    int_code_unit_test();
+    Tests tests = {
+        {"int_code_unit_test",      int_code_unit_test},
+        {"day02_test",              day02_test},
+        {"day03_test",              day03_test},
+        {"day05_test",              day05_test}
+    };
 
-    day02_test();
-    day03_test();
+    for (auto t : tests)
+    {
+        if ( t.test() )
+        {
+            std::cout << "PASSED: ";
+        }
+        else {
+            std::cout << "FAILED: ";
+        }
+        std::cout << t.description << std::endl;
+    }
+
     return 0;
 }
